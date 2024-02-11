a8f7cf08690074212681d7d42ea056e362dbd6bb
junxjr <ruanxj02@gmail.com>
2024-02-10 17:33:26 -0800

    finished workflow file

--- src/counter.py
+++ src/counter.py
@@ -10,17 +10,20 @@ COUNTERS = {}
 # let Flask know that the only methods that is allowed to called
 # on this function is "POST".
 
+
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
 # Create a route for method PUT on endpoint /counters/<name>.
+
+
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
     """update a counter"""
@@ -31,6 +34,8 @@ def update_counter(name):
     return {name: COUNTERS[name]}, status.HTTP_200_OK
 
 # Create a route for method PUT on endpoint /counters/<name>.
+
+
 @app.route('/counters/<name>', methods=['GET'])
 def read_counter(name):
     """read a counter"""
